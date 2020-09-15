2167ffb426e19d7d429ed880bdf2e8cb8f7ba86c
Valeri Gelovani <valeri.gelovani@yahoo.com>
2020-09-15 12:08:44 +0400

    AddNewListing/nextbtnfunction/and property form

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import {TabsetComponent} from 'ngx-bootstrap/tabs/public_api';
 
 @Component({
   selector: 'app-add-property',
@@ -9,6 +10,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AddPropertyComponent implements OnInit {
 @ViewChild('Form') addPropertyForm: NgForm;
+@ViewChild('formTabs') formTabs: TabsetComponent;
+SellRent = '1';
   constructor(private router: Router) { }
 
   // tslint:disable-next-line:typedef
@@ -24,5 +27,9 @@ export class AddPropertyComponent implements OnInit {
   // tslint:disable-next-line:typedef
   onSubmit() {
     console.log(this.addPropertyForm);
+  }
+
+  selectTab(tabId: number) {
+    this.formTabs.tabs[tabId].active = true;
   }
 }

6e1cd8c2cf15467e2af4a9a5e14365a1cac62e53
Valeri Gelovani <valeri.gelovani@yahoo.com>
2020-09-15 10:35:59 +0400

    adding login form

--- Frontend/src/app/user/user-login/user-login.component.ts
+++ Frontend/src/app/user/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
@@ -12,4 +13,7 @@ export class UserLoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  onLogin(loginForm: NgForm){
+   console.log(loginForm.value);
+  }
 }

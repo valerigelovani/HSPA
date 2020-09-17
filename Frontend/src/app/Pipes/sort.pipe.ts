bdb092d9e83316ab45bf8151bde942d33fec117a
Valeri Gelovani <valeri.gelovani@yahoo.com>
2020-09-17 12:31:33 +0400

    CallWebApiToAngular/CittyArrayFromApi

--- Frontend/src/app/Pipes/sort.pipe.ts
+++ Frontend/src/app/Pipes/sort.pipe.ts
@@ -15,7 +15,7 @@ export class SortPipe implements PipeTransform {
     multiplier = -1;
   }
 
-
+    if (value){
     value.sort((a: any, b: any) => {
       if (a[sortField] < b[sortField]) {
       return -1 * multiplier;
@@ -28,4 +28,5 @@ export class SortPipe implements PipeTransform {
     );
     return value;
   }
+}
   }
--- Frontend/src/app/services/housing.service.ts
+++ Frontend/src/app/services/housing.service.ts
@@ -13,6 +13,11 @@ export class HousingService {
 
   constructor(private http: HttpClient) { }
 
+  getAllCities(): Observable<string[]> {
+  return this.http.get<string[]>('http://localhost:5000/api/city');
+  }
+
+
   getProperty(id: number) {
     return this.getAllProperties().pipe(
       map(propertiesArray => {

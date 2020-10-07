36b1525243e96079a96aeb5391574d85627b860f
Valeri Gelovani <valeri.gelovani@yahoo.com>
2020-10-07 16:51:52 +0400

    CreateDBAndFetchDataUsing EntityFrameWork

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -23,7 +23,7 @@ export class AddPropertyComponent implements OnInit {
   // Will come from masters
   propertyTypes: Array<string> = ['სახლი', 'ბინა', 'დუპლექსი'];
   furnishTypes: Array<string> = ['ახალი რემონტი', 'ნახევრად გარემონტებული', 'გასარემონტებელი'];
-  cityList: string[];
+  cityList: any[];
 
   propertyView: IPropertyBase = {
     Id: null,
